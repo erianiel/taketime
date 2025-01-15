7c77faf779cee22cb572ba30fba4e79bcfc0f84d
erianiel <valentina.gravil@gmail.com>
2025-01-15 11:15:04 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-brandFont text-3xl md:text-4xl lg:text-6xl text-red-600">
+        Take Time
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Mulish, monospace",
+    },
+
+    extend: {
+      fontFamily: {
+        brandFont: ["Holtwood\\ One\\ SC"],
+      },
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
+};
