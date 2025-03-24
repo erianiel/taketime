ed4f4e983a8e1240056bba97836055530b1733d6
erianiel <valentina.gravil@gmail.com>
2025-03-24 10:40:18 +0100

    fix: change loader color

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="flex items-center justify-center py-2 backdrop-blur-sm">
-      <div className="aspect-square w-8 animate-spin rounded-full border-4 border-blue-300 border-r-pink-300"></div>
+      <div className="aspect-square w-8 animate-spin rounded-full border-4 border-blue-300 border-r-neutral-300"></div>
     </div>
   );
 }
