2103b315ccf679a6a1ed73fda86ad64df1941c3d
erianiel <valentina.gravil@gmail.com>
2025-03-24 13:17:28 +0100

    chore: add .env file example and implementation

---
+++ src/services/tmdb.js
@@ -0,0 +1,4 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const POSTER_PATH_BASE_URL = import.meta.env.VITE_POSTER_PATH_BASE_URL;
+export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+export const API_KEY = import.meta.env.VITE_API_KEY;
